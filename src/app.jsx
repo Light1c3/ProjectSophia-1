9c287d90bae4d609ce2d7b7a630cd78541a4a731
Elad Karni <ekarni@problemsolutions.net>
2015-12-16 10:40:59 -0800

    Initial Commit HomePage

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      Welcome to Project Sophia!
     </h1>
   }
 });
